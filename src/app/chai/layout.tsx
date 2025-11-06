a9c4408552fbd9a80b81de8e0c4e1992f829c0c2
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-11-06 14:48:36 +0530

    basic nextjs concepts

---
+++ src/app/chai/layout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <h2>inner layout</h2>
+      {children}
+    </>
+  );
+}
